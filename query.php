3d8d19ee0887edbe1bc765c92bd2dc3acde3c1c3
oluwatosin <tosinawe2005@yahoo.co.uk>
2015-04-14 05:23:04 -0400

    Update query.php

--- query.php
+++ query.php
@@ -1,40 +1,60 @@
 <?php
-	
-	$db = new PDO('mysql:host=localhost;dbname=client invoicing problem;charset=utf8', 'root', '');
-	?>
-	<table>
-	<tr>
+
+// this is to set connection variables
+$servername = "localhost";
+$username   = "root";
+$password   = "";
+$dbname     = "client invoicing problem";
+
+// this is to create the connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// this is to create the administrator account
+$dbt->create_admin_account("admin@email.com", "Adm!nP@$$", "Firstname", "Lastname");
+?>
+   <table>
+    <tr>
     <th>Name</th>
     <th>Transactions</th>
-	<th>Billings</th>
+    <th>Billings</th>
   </tr>
-	<?php
-	foreach($db->query('SELECT * FROM client') as $row) {
-		?>
-		<tr>
-		<?php
-		
-		$nTransactions=$row['Transactions'];
-		$nBilling=.50*$nTransactions;
-		if ($nTransactions>50){
-			$nBilling=($nTransactions-50)*.75+.50*50;
-			
-		}
-		?>
-		<td>
-		<?php echo $row['Name'] ?>
-		</td>
-		<td>
-		<?php echo $nTransactions?> 
-		</td>
-		<td>
-		<?php echo $nBilling ?> 
-		</td>
-		</tr>
-		<?php
-	}
-	?>
-	</table>
-	
-
-
+    <?php
+foreach ($db->query('SELECT * FROM client') as $row) {
+?>
+       <tr>
+        <?php
+    //to calculate the transactions
+    $nTransactions = $row['Transactions'];
+    
+    // to calculate the billing
+    $nBilling = .50 * $nTransactions;
+    // if transactions is more than 50 times
+    if ($nTransactions > 50.00) {
+        
+        /**
+         **    this is to calculate the billing rate based on the transactions
+         **/
+        $nBilling = ($nTransactions - 50.00) * .75 + .50 * 50.00;
+        
+    }
+?>
+       <td>
+        <?php
+    echo $row['Name'];
+?>
+       </td>
+        <td>
+        <?php
+    echo $nTransactions;
+?> 
+        </td>
+        <td>
+        <?php
+    echo $nBilling;
+?> 
+        </td>
+        </tr>
+        <?php
+}
+?>
+   </table>
