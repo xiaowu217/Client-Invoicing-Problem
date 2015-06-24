8b1b44e9e9544c8da03467c61416d062bd4f3694
xiaowu217 <qazcz@126.com>
2015-06-24 10:47:59 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <html>
 <head>
-  <title> Client Invoicing Problem </title>
-  <!--script type ="text/javascript" src="/path/to/dojo.js"> </script-->
-  <script>dojoConfig = {async: true, parseOnLoad: false}</script>
-  <script src="//ajax.googleapis.com/ajax/libs/dojo/1.10.4/dojo/dojo.js"></script>
-  <link rel="stylesheet" 
-  href="http://ajax.googleapis.com/ajax/libs/dojo/1.6/dijit/themes/claro/claro.css" />
+    <title> Client Invoicing Problem </title>
+    <!--script type ="text/javascript" src="/path/to/dojo.js"> </script-->
+    <script>dojoConfig = {async: true, parseOnLoad: false}</script>
+    <script src="//ajax.googleapis.com/ajax/libs/dojo/1.10.4/dojo/dojo.js"></script>
+    <link rel="stylesheet" 
+    href="http://ajax.googleapis.com/ajax/libs/dojo/1.6/dijit/themes/claro/claro.css" />
 
 </head>
 <body class="claro">
 
 <?php
-  include('query.php');
+    include('query.php');
 ?>
 
 
