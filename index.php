07679f05017a1fe1b2388b6ab545d441bef61498
xiaowu217 <qazcz@126.com>
2015-06-24 10:07:41 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+  <title> Client Invoicing Problem </title>
+  <!--script type ="text/javascript" src="/path/to/dojo.js"> </script-->
+  <script>dojoConfig = {async: true, parseOnLoad: false}</script>
+  <script src="//ajax.googleapis.com/ajax/libs/dojo/1.10.4/dojo/dojo.js"></script>
+  <link rel="stylesheet" 
+  href="http://ajax.googleapis.com/ajax/libs/dojo/1.6/dijit/themes/claro/claro.css" />
+
+</head>
+<body class="claro">
+
+<?php
+  include('query.php');
+?>
+
+
+</body>
+</html>
