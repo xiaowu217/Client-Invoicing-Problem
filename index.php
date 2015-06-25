df2ea69b5955cf7323cafc0443c5668a3610ffc4
xiaowu217 <qazcz@126.com>
2015-06-25 14:27:52 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,51 @@
 <html>
 <head>
     <title> Client Invoicing Problem </title>
-    <!--script type ="text/javascript" src="/path/to/dojo.js"> </script-->
-    <script>dojoConfig = {async: true, parseOnLoad: false}</script>
-    <script src="//ajax.googleapis.com/ajax/libs/dojo/1.10.4/dojo/dojo.js"></script>
-    <link rel="stylesheet" 
-    href="http://ajax.googleapis.com/ajax/libs/dojo/1.6/dijit/themes/claro/claro.css" />
+	
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	
 </head>
-<body class="claro">
+<body>
 
-<?php
-    include('query.php');
-?>
+	<div class="container">
+		<!--jumbotron for page title-->
+		<div class="jumbotron">
+			<h1>Client Invoicing Problem</h1>
+			<br>
+			<h4>By: Chaozhi Zhang</h2>
+		</div>
+		
+		<hr class="featurette-divider">
+		
+		<div class='panel panel-primary'>
+			<div class='panel-heading'><h4>Client Invoicing Report</h4></div>
+			<div class='panel-body'>
 
+			<?php
+				include('query.php');
+			?>
+			
+			</div>
+		</div>
+		
+		<hr class="featurette-divider">
+		
+		<!-- Footer for copyright-->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; <a href="mailto:zhangcz217@gmail.com">Chaozhi217</a> 2015</p>
+                </div>
+            </div>
+        </footer>
+	</div>
 
 </body>
 </html>
